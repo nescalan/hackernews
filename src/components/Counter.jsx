c5f076b4779a20d0244de1ad374ee50081b942b1
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-06 13:59:13 -0600

    Counter Component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
+//Import Styles
+import "./styles/Counter.css";
+
 const Counter = () => {
   return (
     <div>
-      <h3>This is Counter</h3>
-      <label htmlFor="counter">counter</label>
+      <div className="card card-width">
+        {/* <img src="..." className="card-img-top" alt="..." /> */}
+        <div className="card-body">
+          <h5 className="card-title">
+            <strong>Counter Display</strong>
+          </h5>
+          <p className="card-text">
+            Please, press the Plus (+) to increase the counter or the (-) Button
+            to decrease the counter.
+          </p>
+          <a href="/" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
