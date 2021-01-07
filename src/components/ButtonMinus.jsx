1a66f76468751451fa6cde102c84108dafc4ebf1
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-07 16:17:09 -0600

    Cambio en el titulo <h3> de los botones

--- src/components/ButtonMinus.jsx
+++ src/components/ButtonMinus.jsx
@@ -4,7 +4,7 @@ class ButtonMinus extends Component {
   render() {
     return (
       <div>
-        <h3>This is ButtonMinus</h3>
+        <h3>This is Button Minus</h3>
         <button className="btn btn-success" onClick={this.props.onClick}>
           -
         </button>
--- src/components/ButtonPlus.jsx
+++ src/components/ButtonPlus.jsx
@@ -4,7 +4,7 @@ class ButtonPlus extends Component {
   render() {
     return (
       <div>
-        <h3>This is ButtonMinus</h3>
+        <h3>This is Button Plus</h3>
         <button className="btn btn-success" onClick={this.props.onClick}>
           +
         </button>
